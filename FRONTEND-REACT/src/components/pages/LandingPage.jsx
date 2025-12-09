35dfa981e50ae9f87d4bb8259548542c1d55974e
Eilith404 <minaya.sofia@gmail.com>
2025-12-09 14:16:24 +0100

    Align create card button

--- FRONTEND-REACT/src/components/pages/LandingPage.jsx
+++ FRONTEND-REACT/src/components/pages/LandingPage.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router";
 function LandingPage() {
     return(
         <>
+        <section className="landing">
 
         <Link  to="/create" className="button--link">
             Crear mi tarjeta
         </Link>
+        </section>
         </>
 
     );
 }
 
-export default LandingPage
+export default LandingPage;
