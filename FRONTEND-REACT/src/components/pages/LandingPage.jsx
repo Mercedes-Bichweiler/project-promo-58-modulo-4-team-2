c2d4bdbe75fe2e5245577fb0406e2a1c607c3a97
Eilith404 <minaya.sofia@gmail.com>
2025-12-10 12:16:37 +0100

    Added Card to landing page

--- FRONTEND-REACT/src/components/pages/LandingPage.jsx
+++ FRONTEND-REACT/src/components/pages/LandingPage.jsx
@@ -1,14 +1,48 @@
+import { useState } from "react";
 import { Link } from "react-router";
+import Card from "../create/Card";
 
 function LandingPage() {
-    return(
-        <>
-        <section className="landing">
+    const [projects, setProjects] = useState([{
+        id: 1,
+        name: "",
+        slogan: "",
+        repo: "",
+        demo: "",
+        technologies: "",
+        desc: "",
+        author: "",
+        job: "",
+        photo: "",
+        image: "",
+    },
+    {
+        id: 2,
+        name: "",
+        slogan: "",
+        repo: "",
+        demo: "",
+        technologies: "",
+        desc: "",
+        author: "",
+        job: "",
+        photo: "",
+        image: "",
 
-        <Link  to="/create" className="button--link">
-            Crear mi tarjeta
-        </Link>
-        </section>
+    }])
+    return (
+        <>
+            <section className="landing">
+
+                <Link to="/create" className="button--link">
+                    Crear mi tarjeta
+                </Link>
+                Listado de proyectos
+                {projects.map((project) => (
+                    <Card key={project.id} changeData={project}></Card>
+
+                ))}
+            </section>
         </>
 
     );
